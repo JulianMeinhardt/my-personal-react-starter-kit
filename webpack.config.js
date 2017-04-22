6410c7ed6a9bd63369cade80aeb554dab5aa4cd5
Julian Meinhardt <julianmeinhardt@gmail.com>
2017-04-22 02:19:24 +0200

    changed js files to jsx. Because of linting errors. Added run task to gulpfile and fixed webpack and babel config

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,32 @@
     ./webpack.config.js
 */
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: 'index.html',
+  inject: 'body',
+});
 
 module.exports = {
-  entry: './client/index.js',
+  entry: './src/index.jsx',
   output: {
     path: path.resolve('dist'),
-    filename: 'index_bundle.js',
+    filename: 'main.bundle.js',
   },
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+      {
+        exclude: /node_modules/,
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader',
+      },
     ],
+  },
+  plugins: [HtmlWebpackPluginConfig],
+  resolve: {
+    extensions: ['.js', '.jsx'],
   },
 }
 ;
